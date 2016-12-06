625ecf32261cf6054f9181efc05b4b918679a4e4
makki0086 <masaki.programmer.mydream@gmail.com>
2016-12-06 12:05:24 +0900

    GameManeger作成(途中)
    フォルダを作成し、GameManeger作成
    その他、更新

---
+++ TD-Test-master/Assets/O^hara/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// プレイヤーコントローラー
+/// </summary>
+public class PlayerController : MonoBehaviour {
+    PlayerPositionCounter c;
+    
+    // Use this for initialization
+	void Start () {
+
+
+        c = new PlayerPositionCounter();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        c.PositionCount = c.PositionCount + 1;
+        Debug.Log(c.PositionCount);
+        
+	}
+
+
+}
+
+class PlayerPositionCounter
+{
+    //プレイヤーポジションカウント
+    private int PlayerPositionCount = 1;
+
+    public int PositionCount
+    {
+        get
+        {
+            return PlayerPositionCount;
+        }
+        set
+        {
+            if ((value > 0) && (value < 9))
+            {
+                PlayerPositionCount = value;
+            }
+        }
+    }
+
+}
