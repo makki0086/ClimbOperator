a4d48d7bdbefcbf7e068a81d3383336e30795343
makki0086 <masaki.programmer.mydream@gmail.com>
2017-01-11 14:28:29 +0900

    2017/01/11更新
    タイトルシーン
    キャラクター

--- TD-Test-master/Assets/O^hara/Scripts/PlayStart.cs
+++ TD-Test-master/Assets/O^hara/Scripts/PlayStart.cs
@@ -12,8 +12,25 @@ public class PlayStart : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Began)
+            {
+                // タッチ開始
+                click();
+            }
+            else if (touch.phase == TouchPhase.Moved)
+            {
+                // タッチ移動
+            }
+            else if (touch.phase == TouchPhase.Ended)
+            {
+                // タッチ終了
+            }
+        }
+    }
 
     public void click()
     {
